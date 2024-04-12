41cbe3b833b231043ee2f10e40be46ee6882ea58
きくたたくや <gtk.9223@gmail.com>
2024-04-13 01:37:00 +0900

    タブUI完成

--- src/components/NewList.vue
+++ src/components/NewList.vue
@@ -1,25 +1,31 @@
 <template>
   <ul class="tabs">
-    <li v-for="something in somethingList" :key="something.id" @click="change(something.id)" :class="{ 'isActive': isActive === something.id}">
+    <li v-for="something in somethingList" :key="something.id" @click="change(something.id,something.text)" :class="{ 'isActive': isActive === something.id}">
       タブ{{ something.number }}
     </li>
   </ul>
 
   <!-- 親から子へ -->
   <ul class="contents">
-    <li v-if="isActive">aaa</li>
+    <li v-if="isActive === '1'">{{ defoText }}</li>
+    <li v-else>{{ isText }}</li>
   </ul>
 
 </template>
 
 <script setup lang="ts">
 import { ref, defineProps } from 'vue';
-const isActive = ref("3");
-const change = function(num:string):void {
+const isActive = ref("1");
+const isText = ref();
+
+const change = function(num:string,txt:string):void {
   isActive.value = num;
+  isText.value = txt;
 }
+
 defineProps({
   somethingList: Array,
+  defoText: String
 })
 </script>
 
